b15521d1ba460f218d8cce75d11bfc81f6320add
Surya Jasper <suryajasper2004@gmail.com>
2019-12-26 14:47:34 -0800

    Reorganized variables to access them within the studentAI function

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -6,21 +6,21 @@ struct vec2 {
     int yPos;
 };
 
-static int dir = 0;
-static struct vec2 pos = {0, 0};
-
-static short visited[20][20] = {{0}};
-
 int fixDir(int curr) {
     return curr % 4;
 }
 
-void fixPos() {
+void fixPos(int dir, struct vec2 *pos) {
     struct vec2 move = { (dir % 2 == 0) ? 0 : -(dir-2) , (dir % 2 == 0) ? dir - 1: 0};
-    pos = {pos.xPos + move.xPos, pos.yPos + move.yPos};
+    *pos = {pos->xPos + move.xPos, pos->yPos + move.yPos};
 }
 
 void microMouseServer::studentAI()
 {
+
+    static int dir = 0;
+    static struct vec2 pos = {0, 0};
+
+    static short visited[20][20] = {{0}};
 
 }
