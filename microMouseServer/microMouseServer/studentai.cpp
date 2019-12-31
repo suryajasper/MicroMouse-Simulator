709418589b37adaadc210b14784c47c5419f3df1
Surya Jasper <suryajasper2004@gmail.com>
2019-12-30 22:04:57 -0800

    Bug fixes (mouse now finds the solution)
    - Setting the dead ends to -1 caused the priority checks to fail as they prioritized dead ends over unexplored paths
    - Now, dead ends are considered as walls
    - Solution works perfectly but finding the finish still needs to be implemented

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,5 +1,6 @@
 #include "micromouseserver.h"
 #include <unistd.h>
+#include <limits.h>
 
 struct vec2 {
     int xPos;
@@ -9,7 +10,7 @@ int fixDir(int curr) {
     if (curr < 0) return 4+curr;
     else return curr % 4;
 }
-void fixPos(int dir, struct vec2 *pos, int v[20][20], int marker) {
+void fixPos(int dir, struct vec2 *pos, int v[20][20]) {
     struct vec2 move = { (dir % 2 == 0) ? 0 : -(dir-2) , (dir % 2 == 0) ? -(dir - 1): 0};
     *pos = {pos->xPos + move.xPos, pos->yPos + move.yPos};
     v[pos->xPos][pos->yPos] += 1;
@@ -39,36 +40,45 @@ void microMouseServer::studentAI()
     int leftVal = visited[pos.xPos+left.xPos][pos.yPos+left.yPos];
     int rightVal = visited[pos.xPos+right.xPos][pos.yPos+right.yPos];
     int fwdVal = visited[pos.xPos+fwd.xPos][pos.yPos+fwd.yPos];
+    bool wallLeft = isWallLeft() || leftVal == INT_MAX;
+    bool wallRight = isWallRight() || rightVal == INT_MAX;
+    bool wallForward = isWallForward() || fwdVal == INT_MAX;
 
-    int numWalls = int(isWallLeft()) + int(isWallRight()) + int(isWallForward());
-    bool deadend = ( numWalls == 3 );
+    int numWalls = int(wallLeft) + int(wallRight) + int(wallForward);
     short priority = 0;
     // No choice - deadend
     if (numWalls == 3) {
         backtracking = true;
         turnRight();
         turnRight();
-        visited[pos.xPos][pos.yPos] = -1;
+        visited[pos.xPos][pos.yPos] = INT_MAX;
         dir = fixDir(dir+2);
+        moveForward();
+        fixPos(dir, &pos, visited);
     }
     // Only one choice
     else if (numWalls == 2) {
-        if (!isWallLeft()) {
+        if (!wallLeft) {
             turnLeft();
             dir = fixDir(dir-1);
-        } else if (!isWallRight() ) {
+        } else if (!wallRight ) {
             turnRight();
             dir = fixDir(dir+1);
         }
+        if (backtracking)
+            visited[pos.xPos][pos.yPos] = INT_MAX;
+        moveForward();
+        fixPos(dir, &pos, visited);
     }
     // More than one choice
     else if (numWalls < 2 ) {
+        backtracking = false;
         if (isWallLeft()) {
-            priority = rightVal < fwdVal ? 1 : 2;
-        } else if (isWallRight()) {
-            priority = leftVal < fwdVal ? 0 : 2;
-        } else if (isWallForward()) {
-            priority = leftVal < rightVal ? 0 : 1;
+            priority = rightVal <= fwdVal ? 1 : 2;
+        } else if (wallRight) {
+            priority = leftVal <= fwdVal ? 0 : 2;
+        } else if (wallForward) {
+            priority = leftVal <= rightVal ? 0 : 1;
         }
         if (priority == 0) {
             turnLeft();
@@ -77,38 +87,8 @@ void microMouseServer::studentAI()
             turnRight();
             dir = fixDir(dir+1);
         }
-    }
-    moveForward();
-    fixPos(dir, &pos, visited, visited[pos.xPos][pos.yPos]+1);
-
-/*
-    if (!isWallLeft() && leftVal != -1) {
-        turnLeft();
-        dir = fixDir(dir-1);
-    }
-    else if (!isWallRight() && rightVal != -1) {
-        turnRight();
-        dir = fixDir(dir+1);
-    }
-    if (!isWallForward()) {
         moveForward();
-        if (backtracking && deadEndTest <= 1) {
-            fixPos(dir, &pos, visited);
-        }
-        else {
-            backtracking = false;
-            fixPos(dir, &pos, visited);
-        }
+        fixPos(dir, &pos, visited);
     }
-    else {
-        turnLeft();
-        dir = fixDir(dir-1);
-        visited[pos.xPos][pos.yPos] = -1;
-        backtracking = true;
-    }
-*/
-
-    //printUI(std::to_string(backtracking).c_str());
     printUI(("(" + std::to_string(pos.xPos) + ", " + std::to_string(pos.yPos) + ") curr: " + std::to_string(visited[pos.xPos][pos.yPos]) + " r: " + std::to_string(rightVal) + " l: " + std::to_string(leftVal) + " f: " + std::to_string(fwdVal)).c_str());
-    //printUI(("direction: " + std::to_string(dir)).c_str());
 }
