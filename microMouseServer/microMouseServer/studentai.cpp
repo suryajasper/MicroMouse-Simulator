8f1d0b888d41c5cb4923790e3c0b1ac53354743f
Surya Jasper <suryajasper2004@gmail.com>
2019-12-30 19:06:17 -0800

    Fixed issue with direction and added priority checks

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -4,7 +4,6 @@ struct vec2 {
     int xPos;
     int yPos;
 };
-
 int fixDir(int curr) {
     if (curr < 0) return 4+curr;
     else return curr % 4;
@@ -25,21 +24,27 @@ void microMouseServer::studentAI()
 
     static bool backtracking = false;
 
-    int deadEndTest = 0;
+    struct vec2 left = {0, 0};
+    if (dir % 2 == 0) left = {dir - 1, 0};
+    else left = {0, 2 - dir};
+    struct vec2 right = {-left.xPos, -left.yPos};
+    int leftVal = visited[pos.xPos+left.xPos][pos.yPos+left.yPos];
+    int rightVal = visited[pos.xPos+right.xPos][pos.yPos+right.yPos];
+    short priority = rightVal < leftVal ? 1 : 0;
+    if (rightVal < 0 || leftVal < 0) priority = rightVal < leftVal ? 0 : 1;
+    int deadEndTest = int(!isWallLeft()) + int(!isWallRight()) + int(!isWallForward());
 
-    if (!isWallLeft() && visited[pos.xPos-1][pos.yPos] != -1) {
-        deadEndTest++;
+    if (priority == 0 && !isWallLeft() && leftVal != -1) {
         turnLeft();
         dir = fixDir(dir-1);
     }
-    else if (!isWallRight() && visited[pos.xPos+1][pos.yPos] != -1) {
-        deadEndTest++;
+    else if (!isWallRight() && rightVal != -1) {
         turnRight();
         dir = fixDir(dir+1);
     }
     if (!isWallForward()) {
         moveForward();
-        if (backtracking && ++deadEndTest <= 1) {
+        if (backtracking && deadEndTest <= 1) {
             fixPos(dir, &pos, visited, -1);
         }
         else {
@@ -55,6 +60,8 @@ void microMouseServer::studentAI()
     }
 
     printUI(std::to_string(backtracking).c_str());
-    printUI(("(" + std::to_string(pos.xPos) + ", " + std::to_string(pos.yPos) + ")").c_str());
-    printUI(("direction: " + std::to_string(dir)).c_str());
+    printUI(("rightVal: " + std::to_string(rightVal)).c_str());
+    printUI(("leftVal: " + std::to_string(leftVal)).c_str());
+    //printUI(("(" + std::to_string(pos.xPos) + ", " + std::to_string(pos.yPos) + ")").c_str());
+    //printUI(("direction: " + std::to_string(dir)).c_str());
 }
